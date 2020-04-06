a36166dd244d5e79dd917fc12b36a48fb772250d
Ddystopia <63151578+Ddystopia@users.noreply.github.com>
2020-04-06 15:33:12 +0300

    Add files via upload
    Add cool down 60 minutes to 40 images

--- cmds/increaseMoneyForImage.js
+++ cmds/increaseMoneyForImage.js
@@ -1,8 +1,25 @@
 const Discord = module.require('discord.js');
 const fs = require('fs');
 let cost = 100;
+const games = new Map();
+const lastGames = new Map();
 
 module.exports.run = async (client, message) => {
+
+	if (games.has(message.author.id)) games.set(message.author.id, games.get(message.author.id) + 1);
+	else games.set(message.author.id, 0);
+
+	if (lastGames.has(message.author.id)) {
+		if (Date.now() - lastGames.get(message.author.id) < 60 * 60 * 1000) {
+			lastGames.set(message.author.id, Date.now());
+			games.set(message.author.id, 0);
+		}
+	} else lastGames.set(message.author.id, Date.now());
+
+	const userGames = games.get(message.author.id);
+
+	if(userGames > 40) return;
+
 	const attachmentsNum = message.attachments.size;
 	if (!attachmentsNum) return;
 	let profile;
