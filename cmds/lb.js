9921843f650b00349c2fb556e8f34f9b75e04609
Ddystopia <63151578+Ddystopia@users.noreply.github.com>
2020-04-06 15:15:06 +0300

    Add files via upload
    Update to fix bugs when user want get page more then users on table exist

--- cmds/lb.js
+++ cmds/lb.js
@@ -18,11 +18,13 @@ module.exports.run = async (client, message, args) => {
 		})
 		profiles.sort((a, b) => +b[1].coins - +a[1].coins).forEach(item => lb.push([item[0], item[1].coins]));
 		if (+args[0] < 2) args[0] = 1;
+		if (+args[0] * 10 > lb.length) args[0] = Math.floor(lb.length / 10);
 		if (isNaN(+args[0])) args[0] = 1;
 		embed.setDescription(`Page number ${args[0]--}`);
 		for (let i = 0, index = 0; i < 10; i++) {
+			if(!lb[+`${args[0]}${i}`]) break;
 			const member = message.guild.members.cache.get(lb[+`${args[0]}${i}`][0]);
-			if(!member) continue;
+			if (!member) continue;
 			const username = member.user.username;
 			embed.addField(++index, `${username} - ${lb[+`${args[0]}${i}`][1]}`)
 		}
