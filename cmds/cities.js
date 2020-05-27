7a044ff98a55f8a64e4da669b95bef257367202e
Ddystopia <63151578+Ddystopia@users.noreply.github.com>
2020-05-27 17:48:24 +0300

    fix bugs

--- cmds/cities.js
+++ cmds/cities.js
@@ -4,7 +4,7 @@ let words = [];
 module.exports.run = async (client, message, propArgs) => {
 	if (message.author.bot) return;
 	const args = propArgs || message.content.split(/\s+/g);
-	if(args.length !== 1) return;
+	if (args.length !== 1) return;
 	const word = toFormat(args[0]);
 	switch (word) {
 		case "clear":
@@ -13,15 +13,12 @@ module.exports.run = async (client, message, propArgs) => {
 			break;
 		default:
 			if (!word) return;
-			const correct =
-				!words.length || word[0] === words[words.length - 1].split("").pop();
-			//if first symbol of current word !== last symbol of prev word is false
-			if (words.includes(word) || !correct || word.length < 2) {
-				message.react("❌");
-				message.delete({timeout: 3000});
-			} else {
+			if (isCorrect(word, words)) {
 				words.push(word);
 				message.react("✅");
+			} else {
+				message.react("❌");
+				message.delete({ timeout: 3000 });
 			}
 			break;
 	}
@@ -31,6 +28,18 @@ function toFormat(word) {
 	if (!word) return null;
 	word = word.toLowerCase().replace(/[ьъы]$/g, "");
 	return word;
+}
+
+function isCorrect(word, words) {
+	const correctLastSymbol =
+		!words.length || word[0] === words[words.length - 1].split("").pop();
+	const simpleLanguage = /^[a-z]+$/.test(word) || /^[а-я]+$/.test(word);
+	return (
+		!words.includes(word) &&
+		correctLastSymbol &&
+		word.length > 1 &&
+		simpleLanguage
+	);
 }
 
 module.exports.help = {
