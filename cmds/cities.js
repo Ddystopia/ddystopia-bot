29705b0c95e5ee68907298383c5b22b9d9a02271
Ddystopia <63151578+Ddystopia@users.noreply.github.com>
2020-05-27 17:24:38 +0300

    fix bugs

--- cmds/cities.js
+++ cmds/cities.js
@@ -4,7 +4,8 @@ let words = [];
 module.exports.run = async (client, message, propArgs) => {
 	if (message.author.bot) return;
 	const args = propArgs || message.content.split(/\s+/g);
-	const word = args[0] && args[0].toLowerCase().trim();
+	if(args.length !== 1) return;
+	const word = toFormat(args[0]);
 	switch (word) {
 		case "clear":
 			words = [];
@@ -13,17 +14,24 @@ module.exports.run = async (client, message, propArgs) => {
 		default:
 			if (!word) return;
 			const correct =
-				!words.length || word[0] === words.slice().pop().split("").pop();
+				!words.length || word[0] === words[words.length - 1].split("").pop();
 			//if first symbol of current word !== last symbol of prev word is false
-			if (words.includes(word) || !correct || args[1] || word.length < 2)
+			if (words.includes(word) || !correct || word.length < 2) {
 				message.react("❌");
-			else {
+				message.delete({timeout: 3000});
+			} else {
 				words.push(word);
 				message.react("✅");
 			}
 			break;
 	}
 };
+
+function toFormat(word) {
+	if (!word) return null;
+	word = word.toLowerCase().replace(/[ьъы]$/g, "");
+	return word;
+}
 
 module.exports.help = {
 	name: "cities",
