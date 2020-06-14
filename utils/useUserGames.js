3d02a13b626d3675db03a8707b0c18732d9e10b3
Ddystopia <63151578+Ddystopia@users.noreply.github.com>
2020-06-14 13:49:49 +0300

    refactoring

--- utils/useUserGames.js
+++ utils/useUserGames.js
@@ -1,12 +1,11 @@
 module.exports = useUserGames = (id, games, lastGames) => {
   if (games.has(id)) games.set(id, games.get(id) + 1)
-  else games.set(id, 0)
-
-  if (lastGames.has(id)) {
-    if (Date.now() - lastGames.get(id) > 7 * 60 * 1000) {
-      lastGames.set(id, Date.now())
-      games.set(id, 0)
-    } else lastGames.set(id, Date.now())
+	else games.set(id, 0)
+	
+  const isTimeEnd = Date.now() - lastGames.get(id) > 7 * 60 * 1000
+  if (lastGames.has(id) && isTimeEnd) {
+    lastGames.set(id, Date.now())
+    games.set(id, 0)
   } else lastGames.set(id, Date.now())
 
   return games.get(id)
