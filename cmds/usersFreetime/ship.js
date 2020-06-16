ec1c2a8e04486ab83b19347a486169b8fae3e4f0
Ddystopia <63151578+Ddystopia@users.noreply.github.com>
2020-06-16 22:34:18 +0300

    more ux

--- cmds/usersFreetime/ship.js
+++ cmds/usersFreetime/ship.js
@@ -3,7 +3,10 @@ const randomInteger = require('../../utils/randomInteger')
 const rainbow = require('../../utils/rainbow.js')
 
 module.exports.run = async (client, message, args) => {
-  if (args.length < 2) return
+  const underShipper = args.filter(
+    item => !['и', 'and', '&', '|', '+', '=', '/', '-', '.'].includes(item.toLowerCase())
+  )
+  if (underShipper.length < 2) return
   const embed = new MessageEmbed().setColor(rainbow()).setTitle('Шипперим!!!')
   const percent = randomInteger(0, 100)
   let endPhrase = ''
@@ -15,9 +18,9 @@ module.exports.run = async (client, message, args) => {
   else if (percent > 0) endPhrase = 'им определённо не стоит быть вместе...'
   else if (percent === 0) endPhrase = 'это провал...'
 
-	embed.addField(
+  embed.addField(
     'И что там у нас?',
-    `${args[0]} и ${args[1]} подходят друг другу на ${percent}%, ${endPhrase}`
+    `${underShipper.join(' и ')} подходят друг другу на ${percent}%, ${endPhrase}`
   )
 
   message.reply(embed)
